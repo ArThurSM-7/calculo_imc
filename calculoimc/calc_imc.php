d19351f493fe8f2d46a9abe86cb5a1e2aa6f7a90
Arthur Silva <127902559+ArThurSM-7@users.noreply.github.com>
2023-11-30 12:07:36 -0300

    Update calc_imc.php

--- calculoimc/calc_imc.php
+++ calculoimc/calc_imc.php
@@ -1,25 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtendo os valores do formulário e convertendo para números
+    
     $peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT);
     $alturaRaw = $_POST["altura"];
 
-    // Substituindo vírgula por ponto na altura, caso exista
     $altura = str_replace(',', '.', $alturaRaw);
     
-    // Se não houver ponto ou vírgula na altura, assumimos que está em metros
     if (strpos($altura, '.') === false) {
-        $altura /= 100; // Convertendo para metros
+        $altura /= 100; 
     }
 
     $altura = filter_var($altura, FILTER_VALIDATE_FLOAT);
 
-    // Verificando se os valores são válidos
     if ($peso !== false && $altura !== false && $peso > 0 && $altura > 0) {
-        // Calculando o IMC
+        
         $imc = $peso / ($altura * $altura);
 
-        // Determinando a situação do usuário
         if ($imc < 17) {
             $situacao = "Muito abaixo do peso";
         } elseif ($imc >= 17 && $imc < 18.5) {
@@ -36,7 +32,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $situacao = "Obesidade III (mórbida)";
         }
 
-        // Exibindo resultados
         echo "<h2>Resultado do IMC</h2>";
         echo "<p>Seu IMC é: " . number_format($imc, 2) . "</p>";
         echo "<p>Situação: " . $situacao . "</p>";
